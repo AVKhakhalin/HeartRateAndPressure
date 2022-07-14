d1c7fd409f455131f939f52ad4a88fb33c32f193
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-07-14 16:40:51 +0300

    Реализовано второе приближение сохранения и загрузки данных в FirebaseFirestore. Данные сохраняются и получаются виде даты класса HealthData.

---
+++ app/src/main/java/com/hr/heart/rate/systolic/blood/pressure/firebase/hearrateandpressure/model/HealthData.kt
@@ -0,0 +1,15 @@
+package com.hr.heart.rate.systolic.blood.pressure.firebase.hearrateandpressure.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class HealthData(
+    val nightTime: String,
+    val nightsPressureTop: Int,
+    val nightsPressureBottom: Int,
+    val nightsHeartRate: Int,
+    val daysTime: String,
+    val daysPressureTop: Int,
+    val daysPressureBottom: Int,
+    val daysHeartRate: Int
+)
---
+++ app/src/main/java/com/hr/heart/rate/systolic/blood/pressure/firebase/hearrateandpressure/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.hr.heart.rate.systolic.blood.pressure.firebase.hearrateandpressure.utils
+
+const val MILLISECONDS_NUMBERS: Int = 13
+const val DATA_KEY: String = "data"
+const val HEALTHDATA_KEY: String = "healthData"
+const val DATE_FORMAT: String = "dd/MM/yyyy hh:mm:ss.SSS"
