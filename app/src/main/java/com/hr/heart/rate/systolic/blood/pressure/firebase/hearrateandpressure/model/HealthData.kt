c0f70e04bb27c4c7af282b23c6cae7dd6d5c69c7
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-07-15 17:00:07 +0300

    Реализовано сохранение и отображение вводимых новых данных.

--- app/src/main/java/com/hr/heart/rate/systolic/blood/pressure/firebase/hearrateandpressure/model/HealthData.kt
+++ app/src/main/java/com/hr/heart/rate/systolic/blood/pressure/firebase/hearrateandpressure/model/HealthData.kt
@@ -1,16 +1,18 @@
 package com.hr.heart.rate.systolic.blood.pressure.firebase.hearrateandpressure.model
 
+import com.hr.heart.rate.systolic.blood.pressure.firebase.hearrateandpressure.utils.EMPTY_INT_CODE
+import com.hr.heart.rate.systolic.blood.pressure.firebase.hearrateandpressure.utils.EMPTY_STRING_CODE
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class HealthData(
-    val simpleData: String,
-    val nightTime: String,
-    val nightsPressureTop: Int,
-    val nightsPressureBottom: Int,
-    val nightsHeartRate: Int,
-    val daysTime: String,
-    val daysPressureTop: Int,
-    val daysPressureBottom: Int,
-    val daysHeartRate: Int,
+    val simpleData: String = EMPTY_STRING_CODE,
+    val nightsTime: String = EMPTY_STRING_CODE,
+    val nightsPressureTop: Int = EMPTY_INT_CODE,
+    val nightsPressureBottom: Int = EMPTY_INT_CODE,
+    val nightsHeartRate: Int = EMPTY_INT_CODE,
+    val daysTime: String = EMPTY_STRING_CODE,
+    val daysPressureTop: Int = EMPTY_INT_CODE,
+    val daysPressureBottom: Int = EMPTY_INT_CODE,
+    val daysHeartRate: Int = EMPTY_INT_CODE
 )
